46695d0c75b1d93f3e213d6ecab1bfdef74af905
Edem Tsalah <edem.tsalah@gmail.com>
2016-11-05 12:35:21 +0000

    create a setup script for sqlwrapper

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='sqlwrapper',
+    version='0.0.6',
+    description='generic sql wrapper',
+    author='Edem Tsalah',
+    author_email='edem.tsalah@gmail.com')
