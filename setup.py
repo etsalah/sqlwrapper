79c9eaa66cf50ebf9dd1c422a6909ad1be951a68
Edem Tsalah <edem.tsalah@gmail.com>
2016-11-05 12:49:58 +0000

    working on setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='sqlwrapper',
     version='0.0.6',
     description='generic sql wrapper',
-    scripts=['sqlwrapper.py', ],
+    packages=['sqlwrapper', ],
     author='Edem Tsalah',
     url='https://www.twitter.com/etsalah',
     author_email='edem.tsalah@gmail.com')
