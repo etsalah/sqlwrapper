63dd6f44f8bf6821c4099a9433727a5cac08154b
etsalah <edem.tsalah@gmail.com>
2018-10-01 13:30:35 +0000

    made changes to .gitignore and setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sqlwrapper',
     version='0.0.7',
     description='generic sql wrapper',
-    packages=['sqlwrapper', ],
+    packages=find_packages(),
     author='Edem Tsalah',
     url='https://www.twitter.com/etsalah',
     author_email='edem.tsalah@gmail.com')
